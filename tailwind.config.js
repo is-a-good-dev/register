b3bd55f6ad2fa97f25a1d6bdef5fb47c8305b33a
Will Clarke <clarke19w@gmail.com>
2022-08-06 11:02:38 +0100

    rename old file (temp) & tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors:{
+                "bg":"#181C1F",
+                "accent":"#80C1F5",
+                "text":"#FFFFFF"
+            }
+        }
+    }
+}
