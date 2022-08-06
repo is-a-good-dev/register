dba6876c84d3e41548938043ebc2bf1a487a20a1
Will Clarke <clarke19w@gmail.com>
2022-08-06 11:23:31 +0100

    404 basic content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ tailwind.config = {
         extend: {
             colors:{
                 "bg":"#181C1F",
+                "bg-accent":"#14171C",
                 "accent":"#80C1F5",
                 "text":"#FFFFFF"
             }
