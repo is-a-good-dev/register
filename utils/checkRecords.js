6b9eee3c2435c428e3ab673f5e56caf10af5b6b9
Tweak <tweak@talosbot.xyz>
2022-09-14 17:57:27 -0600

    Update for new format.

--- utils/checkRecords.js
+++ utils/checkRecords.js
@@ -2,10 +2,10 @@ const { checkIfValidIP, checkIfValidFQDN } =  require('./utils.js');
 function checkRecords(data) {
     const recordType = Object.keys(data.target)[0]
     if (recordType.toLowerCase() === "a") {
-        return checkIfValidIP(data.target[recordType])
+        return checkIfValidIP(data.target[recordType].value)
     }
     if (recordType.toLowerCase() === "cname") {
-        return checkIfValidFQDN(data.target[recordType])
+        return checkIfValidFQDN(data.target[recordType].value)
     }
     return false;
 }
