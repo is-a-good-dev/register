d82bceb8b64e9d9648b6ea9c54a92e5f6a3c675b
William Harrison <william@williamdavidharrison.com.au>
2022-11-04 16:46:39 +0800

    add invalid domain check + cleanup

--- utils/checkRecords.js
+++ utils/checkRecords.js
@@ -1,12 +1,16 @@
-const { checkIfValidIP, checkIfValidFQDN } =  require('./utils.js');
+const { checkIfValidIP, checkIfValidFQDN } =  require("./utils.js");
+
 function checkRecords(data) {
-    const recordType = Object.keys(data.target)[0]
-    if (recordType.toLowerCase() === "a") {
-        return checkIfValidIP(data.target[recordType].value)
+    const recordType = Object.keys(data.target)[0];
+
+    if(recordType.toLowerCase() === "a") {
+        return checkIfValidIP(data.target[recordType].value);
     }
-    if (recordType.toLowerCase() === "cname") {
-        return checkIfValidFQDN(data.target[recordType].value)
+
+    if(recordType.toLowerCase() === "cname") {
+        return checkIfValidFQDN(data.target[recordType].value);
     }
     return false;
 }
+
 module.exports = checkRecords;
