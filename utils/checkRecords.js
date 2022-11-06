115979709a66445223d1666b84ced49a8533f096
Tweak <tweak@talosbot.xyz>
2022-11-06 15:36:32 -0700

    Update checkRecords.js

--- utils/checkRecords.js
+++ utils/checkRecords.js
@@ -1,8 +1,10 @@
-const { checkIfValidIP, checkIfValidFQDN } =  require('./utils.js');
+const { checkIfValidIP, checkIfValidFQDN, checkInvalidDomain } =  require('./utils.js');
 
 function checkRecords(data) {
     const recordType = Object.keys(data.target)[0];
-
+    
+    if (checkInvalidDomain(data.target[recordType].name) === true) return false;
+    
     if (recordType.toLowerCase() === 'a') {
         return checkIfValidIP(data.target[recordType].value);
     }
