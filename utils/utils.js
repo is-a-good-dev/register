b615f71695888bcb8e11d99c81afa3b67ee79dc3
Tweak <tweak@talosbot.xyz>
2022-11-06 15:34:32 -0700

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
+const invalidDomains = require('./invalid-domains.json');
+
 module.exports.checkIfValidIP = function(str) {
   // Check if the IP address is valid
   const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
@@ -10,4 +12,8 @@ module.exports.checkIfValidFQDN = function(str) {
   const regexExp = /(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63}$)/gi;
 
   return regexExp.test(str);
+}
+
+module.exports.checkInvalidDomain = function(str) {
+  return invalidDomains.includes(str);
 }
