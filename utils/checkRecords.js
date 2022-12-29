48ed3dd0af3e749a234b2efb914bbaf32d7d7faf
William Harrison <william@williamdavidharrison.com.au>
2022-12-29 18:20:12 +0800

    Update checkRecords.js

--- utils/checkRecords.js
+++ utils/checkRecords.js
@@ -2,15 +2,23 @@ const { checkIfValidIP, checkIfValidFQDN, checkInvalidDomain } =  require('./uti
 
 function checkRecords(data) {
     const recordType = Object.keys(data.target)[0];
-    
+
+    // Check if the domain is invalid
     if (checkInvalidDomain(data.target[recordType].name) === true) return false;
-    
+
+    // Check A record
     if (recordType.toLowerCase() === 'a') {
         return checkIfValidIP(data.target[recordType].value);
     }
 
+    // Check CNAME record
     if (recordType.toLowerCase() === 'cname') {
         return checkIfValidFQDN(data.target[recordType].value);
+    }
+
+    // Check if record name matches file name
+    if (recordType.toLowerCase() === 'a' || recordType.toLowerCase() === 'cname') {
+        if (data.target[recordType].name != /* Put file name value here */) return false;
     }
 
     return false;
