d10f2d91f6fa9d59eeba0a37cb94f167b2bae3b0
Tweak <tweak@talosbot.xyz>
2023-01-19 18:21:37 -0700

    Update addRecord.js to support a records in array.

--- scripts/addRecords.js
+++ scripts/addRecords.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const getJSON = require('../utils/getJSON.js');
 const data = getJSON(process.env.FILES);
-
+const arrayRecordTypes["
 async function addRecord(type, name, value) {
   const response = await fetch(`https://api.is-a-good.dev/api/is-a-good-dev/zones/add?apiKey=${process.env.API_KEY}&type=${type}&name=${name}&content=${value}`);
   const data = await response.json();
@@ -14,14 +14,19 @@ async function addRecord(type, name, value) {
       if (!data) return;
 
       const records = Object.keys(data.target);
-
       for (const i in records) {
-        recordType = records[i];
-        name = data.target[recordType].name;
-        value = data.target[recordType].value;
-
-        const res = await addRecord(recordType, name, value);
-        console.log(res);
+        const recordType = records[i];
+        const name = data.target[recordType].name;
+        const value = data.target[recordType].value;
+        console.log(`Adding record type ${recordType} with values ${name}, ${value}`);
+        if (arrayRecordTypes.includes(recordType.toLowerCase()) != true) {
+          let res = await addRecord(recordType, name, value);
+          return console.log(res);
+        }
+        for (const ip of value) {
+          let res = await addRecord(recordType, name, ip);
+          console.log(res);
+        }
       }
     } catch(e) {
       console.log('Failed to add records.');
