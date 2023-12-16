3db7e77fcb18c3cfa152b882aa5dc43a7ffc831d
vader113 <144384935+vader113@users.noreply.github.com>
2023-12-17 00:21:41 +0500

    Create techngeex.js

---
+++ sub-logs/techngeex.js
@@ -0,0 +1,17 @@
+{
+  "repo": "https://github.com/vader113/register",
+
+  "owner": {
+    "username": "vader113",
+    "email": "saqibjarvis11+1@gmail.com"
+  },
+
+  "target": {
+    "NS": {
+       "name": "techngeex", 
+       "value": "ns1.byet.org"
+     }
+  },
+
+  "proxied": false
+}
