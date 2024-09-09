0e0cc45f6b437a864d0820f7fc20fef37b951571
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-09-08 22:07:59 -0400

    chore: removing it from another file

--- utils/getJSON.js
+++ utils/getJSON.js
@@ -15,16 +15,6 @@ function getJSON(file, filename) {
       const rawdata = fs.readFileSync(path); // Read the file
       const data = JSON.parse(rawdata); // Parse it
       
-      // Extract the file name without extension
-      const fileNameWithoutExt = stripExt(file);
-      
-      // Validate if the file name matches any of the target.RECORD_TYPE values
-      const recordTypes = Object.keys(data.target);
-      if (!recordTypes.includes(fileNameWithoutExt.toUpperCase())) {
-        core.setOutput('infoMessage', `File name ${fileNameWithoutExt} does not match any target.RECORD_TYPE`);
-        return false;
-      }
-      
       return data; // Return the parsed data if validation passes
     }
     return 3; // It doesn't exist
