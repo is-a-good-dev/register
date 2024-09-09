6b8684fbc3478354139670cd58aeaa3dd637e14d
Tweak <tweak@talosbot.xyz>
2024-09-08 21:26:15 -0600

    Update getFileName.js to remove invalid code

--- utils/getFileName.js
+++ utils/getFileName.js
@@ -7,7 +7,6 @@ async function getFileName(file, data) {
       // Validate if the file name matches any of the target.RECORD_TYPE values
       const recordTypes = Object.keys(data.target);
       if (!recordTypes.includes(fileNameWithoutExt.toUpperCase())) {
-        core.setOutput('infoMessage', `File name ${fileNameWithoutExt} does not match any target.RECORD_TYPE`);
         return false;
       } else {
         return true;
