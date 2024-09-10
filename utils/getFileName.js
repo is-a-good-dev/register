1be98644b1df685aa1c0bd66a6f7c77a8839b439
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-09-10 16:21:31 -0400

    chore: some debugging stuff

--- utils/getFileName.js
+++ utils/getFileName.js
@@ -2,10 +2,14 @@ const { stripExt } = require('./utils.js');
 
 async function getFileName(file, data) {
       // Extract the file name without extension
+      console.log(file);
+      console.log(data);
+
       const fileNameWithoutExt = stripExt(file);
       
       // Validate if the file name matches any of the target.RECORD_TYPE values
       const recordTypes = Object.keys(data.target);
+
       if (!recordTypes.includes(fileNameWithoutExt.toUpperCase())) {
         return false;
       } else {
