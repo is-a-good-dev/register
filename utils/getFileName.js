ffb0038e54d494c5347bac1c493632a2dd08267b
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-09-20 21:46:34 -0400

    fix: validation

--- utils/getFileName.js
+++ utils/getFileName.js
@@ -1,20 +1,13 @@
 const { stripExt } = require('./utils.js');
 
-async function getFileName(file, data) {
-      // Extract the file name without extension
-      console.log(file);
-      console.log(data);
+function getFileName(file, data) {
 
       const fileNameWithoutExt = stripExt(file);
       
       // Validate if the file name matches any of the target.RECORD_TYPE values
-      const recordTypes = Object.keys(data.target);
+      const recordTypes = Object.values(data.target);
 
-      if (!recordTypes.includes(fileNameWithoutExt.toUpperCase())) {
-        return false;
-      } else {
-        return true;
-      }
+      return recordTypes[0].name == fileNameWithoutExt.toLowerCase();
 }
 
 module.exports = getFileName;
--- utils/getJSON.js
+++ utils/getJSON.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const { getFileExtension, stripExt } = require('./utils.js');
 const core = require('@actions/core');
 
-function getJSON(file, filename) {
+function getJSON(file) {
+
   const path = `${process.env.actions_path}/${file}`; // File path.
+  
   const ext = getFileExtension(file);
   if (path.includes("sub-logs") && !ext || ext != 'json') return 3 //if file is subdomain file but has no ext or diff ext than .json
   if (!ext) return false; // If no file extension, return.
@@ -11,6 +13,7 @@ function getJSON(file, filename) {
   
   try {
     if (fs.existsSync(path)) { // Check if file exists in domain directory
+
       // It exists
       const rawdata = fs.readFileSync(path); // Read the file
       const data = JSON.parse(rawdata); // Parse it
@@ -19,6 +22,7 @@ function getJSON(file, filename) {
     }
     return 3; // It doesn't exist
   } catch(err) {
+
     core.setOutput('infoMessage', "Could not validate info.");
     core.setOutput('recordMessage', "Could not validate records.");
     core.setOutput('jsonData', "Could not read the JSON file, did you have an error in your syntax?")
