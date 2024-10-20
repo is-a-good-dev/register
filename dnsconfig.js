166b84c6221d5955091bb75762d268956bae0e90
Tweak4141 <tweak@talosbot.xyz>
2024-10-19 22:40:49 -0600

    Debug

--- dnsconfig.js
+++ dnsconfig.js
@@ -52,6 +52,7 @@ for (var subdomain in allDomains) {
         if (Array.isArray(domainData.target.TXT.value)) {
             for (var txt in domainData.target.TXT) {
                 var txtRecord = domainData.target.TXT[txt];
+                console.log(txtRecord.name, txtRecord.value)
                 commit.push(TXT(domainData.target.TXT.name === "@" ? subdomainName : txtRecord.name + "." + subdomainName, txtRecord.value));
             }
         } else {
